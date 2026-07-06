d430206199167a5ba1cc35824690bb1dc767e554
Kurtis <kurtis@Kurtiss-MacBook-Pro.local>
2026-07-06 17:56:44 -0400

    initial nix config

---
+++ flake.nix
@@ -0,0 +1,17 @@
+{
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+    home-manager.url = "github:nix-community/home-manager";
+    home-manager.inputs.nixpkgs.follows = "nixpkgs";
+  };
+
+  outputs = { nixpkgs, home-manager, ... }:
+  let
+    system = "aarch64-darwin";   # Apple Silicon. Use "x86_64-darwin" on Intel Macs.
+  in {
+    homeConfigurations.kurtis = home-manager.lib.homeManagerConfiguration {
+      pkgs = nixpkgs.legacyPackages.${system};
+      modules = [ ./home.nix ];
+    };
+  };
+}
---
+++ home.nix
@@ -0,0 +1,36 @@
+{ pkgs, ... }:
+{
+  home.username = "kurtis";
+  home.homeDirectory = "/Users/kurtis";
+  home.stateVersion = "24.11";   # leave this; it pins compatibility
+  programs.home-manager.enable = true; 
+
+  # ── Your global toolbelt: add/remove tools here ──
+  home.packages = with pkgs; [
+    git
+    jq
+    eza
+  ];
+
+#*** everyday loop, edit home.nix then run: 
+#*** home-manager switch --flake ~/.config/home-manager#kurtis
+
+  # We declare "~/.local/bin should be on my PATH" HERE, up front.
+  home.sessionPath = [ "$HOME/.local/bin" ];
+
+  # Home Manager owns your shell (this creates ~/.zshrc for you).
+  programs.zsh = {
+  enable = true;                # you already have this
+  shellAliases = {
+    ls = "eza --icons=always -a --group-directories-first";
+    ggpush = "git push";            
+  };
+};
+
+  # direnv powers the per-project layer in Step 5.
+  # Because zsh is managed above, the direnv hook is wired in automatically.
+  programs.direnv = {
+    enable = true;
+    nix-direnv.enable = true;
+  };
+}
